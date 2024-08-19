3365270a6b86302e81234ca2fea3b1b9dc8475c7
Nerdzter <nayderson.silva.oliveira@gmail.com>
2024-08-18 21:24:59 -0300

    Aprendendo usar o JFRAME no java

---
+++ src/Screen.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Screen extends JFrame{
+    
+    public Screen (){
+
+        setVisible(true);
+        setSize(1280, 720);
+        setTitle("Aprendendo JFrame");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
